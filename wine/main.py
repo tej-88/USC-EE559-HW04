a4a9cba7431e821a6852c16dba33ca5e6bfafb00
Tejas <tejas8acharya@gmail.com>
2023-06-16 16:49:17 +0530

    Add load the data to main.py

--- wine/main.py
+++ wine/main.py
@@ -8,3 +8,12 @@ import matplotlib.pyplot as plt
 #Constants
 TRAIN_DATA_FILENAME = './wine_train.csv'
 TEST_DATA_FILENAME = './wine_test.csv'
+
+#Load the Data
+def load_data(filename):
+    data = pd.read_csv(filename, header=None)
+    X = data.iloc[:, 0:13].to_numpy()
+    X0 = np.ones((X.shape[0], 1))
+    X = np.concatenate((X0, X), axis=1)
+    y = data.iloc[:, -1].to_numpy() - 1
+    return X, y.reshape((y.shape[0], 1))

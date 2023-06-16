1de9e887de208f418b0bb91df5c213fe23035b24
Tejas <tejas8acharya@gmail.com>
2023-06-16 16:53:44 +0530

    Add fit method for Perceptron

--- wine/perceptron.py
+++ wine/perceptron.py
@@ -6,3 +6,22 @@ class Perceptron():
     def __init__(self, eta, i_max):
         self.eta = eta
         self.I_MAX = i_max
+
+    def fit(self, X, y, is_version_1=True):
+        self.i = 0
+        self.C = len(np.unique(y))
+        self.D, self.N = X.shape
+        self.weights = []
+
+        for i in range(self.C):
+            self.weights.append(np.ones((1, self.D)))
+            
+        data = np.concatenate((X, y), axis=1)
+
+        if (is_version_1):
+            self.sgd_1(data)
+        else:
+            self.sgd_2(data)
+        
+        return
+    
